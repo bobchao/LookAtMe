d78ff53477dd52b7181c2dfcf922182313373fd0
Po-chiang Chao <bobchao@gmail.com>
2024-10-27 16:07:57 +0800

    fix hexToRgb

--- script.js
+++ script.js
@@ -84,9 +84,9 @@ document.addEventListener('DOMContentLoaded', () => {
     function hexToRgb(hex) {
         const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         return result ? {
-            r: parseInt(result[1], 16) / 255,
-            g: parseInt(result[2], 16) / 255,
-            b: parseInt(result[3], 16) / 255
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
         } : null;
     }
 
