b3f7249ab08cfcc225cebade0cff1749c64513ba
Po-chiang Chao <bobchao@gmail.com>
2024-10-27 16:10:47 +0800

    Change update method

--- sw.js
+++ sw.js
@@ -14,12 +14,32 @@ self.addEventListener('install', (event) => {
       ]);
     })
   );
+  self.skipWaiting(); // 立即控制頁面
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'v1') {
+            return caches.delete(cacheName); // 刪除舊的快取
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, fetchResponse.clone()); // 更新快取
+          return fetchResponse;
+        });
+      });
     })
   );
 });
